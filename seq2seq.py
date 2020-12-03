d48447e64fd873a250ad3d335e7d895aa037f4be
Abdelrahman Elsayed <abdoelsayed2016@gmail.com>
2020-12-03 23:13:35 +0600

    decodergru

--- seq2seq.py
+++ seq2seq.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class GRUEncodeer(nn.module):
+class GRUEncodeer(nn.Module):
     def __init__(self,input_size,embedding_size,hidden_size):
 
 
@@ -25,3 +25,23 @@ class GRUEncodeer(nn.module):
     def init_hidden(self):
         return torch.zeros(1,1,self.hidden_size)
 
+class GRUDecoder(nn.Module):
+    def __init__(self,output_size,embedding_size,hidden_size):
+        super(GRUDecoder,self).__init__()
+        self.output_size=output_size
+        self.embedding_size=embedding_size
+        self.hidden_size=hidden_size
+
+        self.embedding=nn.Embedding(output_size,embedding_size)
+        self.gru=nn.GRU(embedding_size,hidden_size,bidirectional=False,batch_first=True)
+        self.out=nn.Linear(hidden_size,output_size)
+        self.softmax=nn.LogSoftmax(dim=1)
+    def forward(self,input,hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+
+        output, hidden = self.gru(output, hidden)
+
+        output = self.softmax(self.out(output[0]))
+
+        return output, hidden
